60b88c913bb92808f357b67d0b9ed69f655fdd2c
anatoly kozlov <kozlof9@yandex.ru>
2018-10-15 14:34:07 +0300

    new classes added

--- MIPT_Compiler/Analyzer/BisonUtils.h
+++ MIPT_Compiler/Analyzer/BisonUtils.h
@@ -3,6 +3,10 @@
 
 using exst = Expression::ExpressionState;
 using boot = BinaryOperator::OperationType;
+using titt = TypeIdentifier::TypeType;
+using sst = Statement::StatementType;
+using aas = Argument::ArgumentState;
+using mv = Modifier::Value;
 
 inline Expression* ToExpr(Tree* node)
 {
@@ -27,4 +31,34 @@ inline Value* ToVal(Tree* node)
 inline Identifier* ToID(Tree* node)
 {
 	return static_cast<Identifier*>(node);
+}
+
+inline Statement* ToState(Tree* node)
+{
+	return static_cast<Statement*>(node);
+}
+
+inline TypeIdentifier* ToType(Tree* node)
+{
+	return static_cast<TypeIdentifier*>(node);
+}
+
+inline VarDeclaration* ToVar(Tree* node)
+{
+	return static_cast<VarDeclaration*>(node);
+}
+
+inline Argument* ToArg(Tree* node)
+{
+	return static_cast<Argument*>(node);
+}
+
+inline Modifier* ToMod(Tree* node)
+{
+	return static_cast<Modifier*>(node);
+}
+
+inline MethodDeclaration* ToFunc(Tree* node)
+{
+	return static_cast<MethodDeclaration*>(node);
 }
--- MIPT_Compiler/Analyzer/Entities.h
+++ MIPT_Compiler/Analyzer/Entities.h
@@ -15,6 +15,20 @@ protected:
 
 class Expression;
 
+class Argument;
+
+class Modifier : public Tree
+{
+public:
+	enum Value
+	{
+		PT_Public,
+		PT_Private
+	} value;
+
+	Modifier(Value val): value(val) {}
+};
+
 class Value : public Tree
 {
 public:
@@ -44,7 +58,44 @@ public:
 	Identifier(std::string str): name(str) {}
 };
 
-class Statement: public Tree {};
+class Statement: public Tree
+{
+public:
+	Statement* statement;
+	Expression* expr1;
+	Expression* expr2;
+	Statement* elseState;
+	Identifier* id;
+
+	enum StatementType
+	{
+		SHADE_TYPE,
+		IF_TYPE,
+		WHILE_TYPE,
+		PRINT_TYPE,
+		ASSIGN_TYPE,
+		ARR_ASSIGN_TYPE,
+		LIST_TYPE,
+		EMPTY_TYPE,
+	} type;
+
+	Statement(StatementType state): type(state) {};
+	Statement(Statement* st, StatementType state) : statement(st), type(state) {};
+	Statement(Expression* condition, Statement* st, Statement* elseSt, StatementType state)
+			: statement(st),
+			expr1(condition),
+			elseState(elseSt),
+			type(state) {};
+	Statement(Expression* condition, Statement* st, StatementType state) : statement(st), expr1(condition), type(state) {};
+	Statement(Expression* expres, StatementType state) : expr1(expres), type(state) {};
+	Statement(Expression* expres, Identifier* ident, StatementType state) : id(ident), expr1(expres), type(state) {};
+	Statement(Expression* expres, Identifier* ident, Expression* value, StatementType state) 
+			: id(ident),
+			expr1(expres),
+			expr2(value),
+			type(state) {};
+
+};
 
 class FunctionCall : public Tree
 {
@@ -96,7 +147,8 @@ public:
 		NewObj_STATE,
 		Not_STATE,
 		List_STATE,
-		Empty_STATE
+		Empty_STATE,
+		RETURN_STATE
 	} state;
 
 	Expression(Value* a, ExpressionState st) : value(a), state(st) {}
@@ -115,10 +167,38 @@ public:
 	explicit Expression(ExpressionState st) : state(st) {}
 };
 	
+class TypeIdentifier : public Tree
+{
+public:
+	// :)
+	enum TypeType
+	{
+		INT_TYPE,
+		BOOL_TYPE,
+		INTA_TYPE,
+		USER_TYPE
+	} type;
+	Identifier* id;
 
-class TypeIdentifier : public Tree {};
+	TypeIdentifier(TypeType tp): type(tp) {}
 
-class MethodDeclaration : public Tree {};
+	TypeIdentifier(Identifier* userType, TypeType tp) : type(tp), id(userType) {}
+};
+
+class MethodDeclaration : public Tree
+{
+public:
+	Modifier* modifier;
+	Identifier* name;
+	Argument* arguments;
+	Statement* body;
+
+	MethodDeclaration(Modifier* visibility, Identifier* id, Argument* args, Statement* stats)
+		: modifier(visibility),
+		name(id),
+		arguments(args),
+		body(stats) {}
+};
 
 class ClassDeclaration : public Tree {};
 
@@ -126,7 +206,33 @@ class MainClass : public Tree {};
 
 class Goal : public Tree {};
 
-class VarDeclaration : public Tree {};
+class VarDeclaration : public Tree
+{
+public:
+	TypeIdentifier* type;
+	Identifier* id;
 
+	VarDeclaration(TypeIdentifier* tid, Identifier* name): type(tid), id(name) {}
+};
 
+class Argument : public Tree
+{
+public:
+	enum ArgumentState
+	{
+		EMPTY_STATE,
+		LAST_STATE,
+		ONE_STATE,
+		LIST_STATE,
+		MAIN_STATE
+	} state;
+
+	Argument* arg;
+	VarDeclaration* var;
+	Argument(ArgumentState st): state(st) {}
+
+	Argument(VarDeclaration* variab, ArgumentState st): var(variab), state(st) {}
+
+	Argument(Argument* argum, VarDeclaration* variab, ArgumentState st): var(variab), arg(argum), state(st) {}
+};
 

78f56503599ff1a87ff9014f0fcadc121d000134
Дмитрий Родин <rodin.dv@phystech.edu>
2018-12-02 23:10:57 +0300

    Таблица с ошибками

--- MIPT_Compiler/Analyzer/ErrorTable.h
+++ MIPT_Compiler/Analyzer/ErrorTable.h
@@ -1,10 +1,19 @@
 #pragma once
 #include <string>
 #include <UndefinedTypeException.h>
+#include <vector>
+#include <string>
 
 class CErrorTable {
 public:
 	static void AddError(std::string message);
 	static void AddError(CUndefinedTypeException* error);
+	static void Print(std::fstream& stream);
 
+private:
+	CErrorTable() {}
+	static void createIfNotNull();
+	static std::shared_ptr<CErrorTable> instance;
+
+	std::vector<std::string> errorMessages;
 };

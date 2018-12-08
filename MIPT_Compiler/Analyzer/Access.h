c7854f7f99a41af7ab4f0026121845a2e42e726b
anatoly kozlov <kozlof9@yandex.ru>
2018-12-08 17:19:07 +0300

    Revert "some impls"
    This reverts commit fb02d5f513b032ce0c045ba66952c344763f3128.

--- MIPT_Compiler/Analyzer/Access.h
+++ MIPT_Compiler/Analyzer/Access.h
@@ -6,17 +6,9 @@ public:
 };
 
 class CInRegAccess : public IAccess {
-public:
-	CInRegAccess(int _reg);
-	int GetReg() const;
-private:
-	int reg;
+
 };
 
 class CInFrameAccess : public IAccess {
-public:
-	CInFrameAccess(int _offset);
-	int GetOffset() const;
-private:
-	int offset;
+
 };
--- MIPT_Compiler/Analyzer/Frame.h
+++ MIPT_Compiler/Analyzer/Frame.h
@@ -2,9 +2,7 @@
 
 #include <Symbol.h>
 #include <Access.h>
-#include <Type.h>
 #include <vector>
-#include <map>
 
 class IFrame {
 public:
@@ -16,34 +14,29 @@ public:
 	virtual int GetTotalSize() const = 0;
 };
 
+class CNestedFrame : public IFrame {
+public:
+	virtual void AddFormal(const CSymbol* name) = 0;
+	virtual void AddLocal(const CSymbol* name) = 0;
+	virtual int FormalsCount() const = 0;
+	virtual const IAccess* GetFormal(int index) const = 0;
+	virtual const IAccess* FindFormalorLocal(const CSymbol* name) const = 0;
+	virtual int GetTotalSize() const = 0;
+};
+
 class CMiniJavaFrame : public IFrame {
 public:
-	virtual void AddFormal(const CSymbol* name, const CType* type);
-	virtual void AddLocal(const CSymbol* name, const CType* type);
-	virtual int FormalsCount() const;
-	virtual const IAccess* GetFormal(int index) const;
-	virtual const IAccess* FindFormalorLocal(const CSymbol* name) const;
+	virtual void AddFormal(const CSymbol* name) = 0;
+	virtual void AddLocal(const CSymbol* name) = 0;
+	virtual int FormalsCount() const = 0;
+	virtual const IAccess* GetFormal(int index) const = 0;
+	virtual const IAccess* FindFormalorLocal(const CSymbol* name) const = 0;
 	void Finalize();
-	virtual int GetTotalSize() const;
-
-	static int GetOffset(TDataType type);
-protected:
+	virtual int GetTotalSize() const = 0;
+private:
 	int currOffset = 0;
 	std::vector< std::unique_ptr<const IAccess> > formals;
 	std::vector< std::unique_ptr<const IAccess> > locals;
-	std::map< const CSymbol*, const IAccess* > namesMap;
-	bool isFinalized = false;
-};
-
-class CNestedFrame : public CMiniJavaFrame {
-public:
-
-};
-
-
-
-class CFunctionFrame : public CMiniJavaFrame {
-public:
-private:
-
+	std::vector< std::unique_ptr<const CNestedFrame> > nested;
+	bool isFinalized;
 };
